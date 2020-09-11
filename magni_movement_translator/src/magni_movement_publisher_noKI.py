85989dc952aae19b4dd69d0ef62ea4557b57209c
Alex Beh <chunjye088@gmail.com>
2020-09-11 09:04:11 +0800

    fine tuning movement_translator params

--- magni_movement_translator/src/magni_movement_publisher_noKI.py
+++ magni_movement_translator/src/magni_movement_publisher_noKI.py
@@ -28,7 +28,7 @@ class MagniMovementPublisherNode():
         rospy.init_node("magni_movement_publisher_node")
         self.magni_movement_subscriber = rospy.Subscriber("/aruco_single/position", person_goal, self.magni_position_callback)
 
-        self.magni_movement_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        self.magni_movement_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=0)
        
         self.previous_time = 0
         self.sum_error_r = 0
@@ -38,11 +38,11 @@ class MagniMovementPublisherNode():
         self.Ki_linear_window  = 0.95
         self.Ki_angular_window = 0.95
         # Parameters in simulation: self.linear_bound = 0.3 self.angular_bound = 0.4
-        self.linear_bound  = 0.3
+        self.linear_bound  = 0.4
         self.angular_bound = 0.4
         self.following_distance = rospy.get_param('~following_distance', 1.0)
-        self.dead_zone_radius   = rospy.get_param('~dead_zone_radius',  0.2) # +/- m
-        self.dead_zone_theta    = rospy.get_param('~dead_zone_theta', 0.1) # +/- rad
+        self.dead_zone_radius   = rospy.get_param('~dead_zone_radius',  0.3) # +/- m
+        self.dead_zone_theta    = rospy.get_param('~dead_zone_theta', 0.15) # +/- rad
         self.Kp_linear  = rospy.get_param('~Kp_linear', 0.8)
         self.Kp_angular = rospy.get_param('~Kp_angular', 1.5)
         self.Ki_linear  = rospy.get_param('~Ki_linear', 0.0)
@@ -55,50 +55,38 @@ class MagniMovementPublisherNode():
     def magni_position_callback(self, data):
         # print(data.vector.x, data.vector.y, data.vector.z)
         pointstamped_msg = PointStamped()
-        pointstamped_msg.header.frame_id = "odom"
+        pointstamped_msg.header.frame_id = "base_footprint"
         pointstamped_msg.header.stamp =rospy.Time(0)
         pointstamped_msg.point.x = data.goal.vector.x
         pointstamped_msg.point.y = data.goal.vector.y
         pointstamped_msg.point.z = 0
 
-        transform_state = False
-
-        # previous_time = rospy.Time.now().to_sec()
-        try:
-            # transform the tracked_person from odom from to base_link
-            self.listener.waitForTransform("/base_link", "/odom", rospy.Time(0),rospy.Duration(4.0))
-            transform_state = True
-            transformed_point=self.listener.transformPoint("base_link",pointstamped_msg)
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-            # print("Transformation got error!")
-            print(e)
-            # self.magni_movement_publisher.publish(Twist())
-            return
-        # print(rospy.Time.now().to_sec() - previous_time)
-
-        r,th = self.xyToPolar(transformed_point)
+        r,th = self.xyToPolar(pointstamped_msg)
 
         current_time = rospy.Time.now().to_sec()
         dt = current_time - self.previous_time
-
+	
         # Calculate PI control
+        # if r > self.following_distance :
         error_r  = r - self.following_distance
+        
         error_th = th - 0.0
         
         if abs(error_r) >= self.dead_zone_radius:
+            print("linear")
+            # follow_speed_x = error_r 
             self.sum_error_r  = self.Ki_factor_linear*self.Ki_linear_window*self.sum_error_r + error_r*dt
             follow_speed_x = self.Kp_linear * error_r + self.Ki_linear  * (self.sum_error_r)
-            # print("linear")
             follow_speed_x = self.constrain(follow_speed_x, self.linear_bound, -self.linear_bound)
         else:
             follow_speed_x = 0.0
             # rospy.loginfo('Linear Dead Zone!')
 
         if abs(error_th) >= self.dead_zone_theta:
-            # print("angular")
+            print("angular")
+            follow_speed_z = error_th
             # self.sum_error_th = self.Ki_factor_angular*self.Ki_angular_window*self.sum_error_th + error_th*dt
             # follow_speed_z = self.Kp_angular * (th)   + self.Ki_angular * (self.sum_error_th)
-            follow_speed_z = error_th
             follow_speed_z = self.constrain(follow_speed_z, self.angular_bound, -self.angular_bound)
         else:
             follow_speed_z = 0.0
@@ -106,27 +94,22 @@ class MagniMovementPublisherNode():
         
         # Pub vel cmd
         motion_cmd = Twist()
-        # transform_state
-        if transform_state:
-            #Parameters in simulation: ratio_angular_speed = 1.5 && ratio_linear_speed = 1 && vector_tf = 1
-            ratio_angular_speed = 0.8
-            ratio_linear_speed = 1
 
-            vector_tf = -1
-            add_person_twist = 1
+        #Parameters in simulation: ratio_angular_speed = 1.5 && ratio_linear_speed = 1 && vector_tf = 1
+        ratio_angular_speed = 1.1
+        ratio_linear_speed = 1.0
 
-            motion_cmd.angular.z = follow_speed_z*ratio_angular_speed
-            # if(data.robot_heading.data ==1):
-            motion_cmd.linear.x  = (follow_speed_x*ratio_linear_speed + add_person_twist*0.3*data.goal.vector.z)*vector_tf
-            # else:
-            #     print("move_back")
-            #     motion_cmd.linear.x  = -0.3
-        else:
-            print("ERROR in transform_state.\n")
+        vector_tf = -1
+        add_person_twist_linear = 1
+        add_person_twist_angular = 0
 
-        # print("---> {}".format(motion_cmd.linear.x))
+        if r > self.following_distance :    
+            motion_cmd.linear.x  = (follow_speed_x*ratio_linear_speed + add_person_twist_linear*0.3*data.goal.vector.z)*vector_tf
+        
+        # if abs(follow_speed_z) > 0.1:
+        motion_cmd.angular.z = follow_speed_z*ratio_angular_speed + add_person_twist_angular*0.3*math.copysign(data.goal.vector.z,follow_speed_z)
+
         self.magni_movement_publisher.publish(motion_cmd)
-        # print(datetime.now())
         self.previous_time = current_time
 
     def constrain(self, num, limit_upper, limit_lower):
@@ -143,10 +126,10 @@ class MagniMovementPublisherNode():
 
     def xyToPolar(self,data_xyz):
         # change here for different transformation
+        # longitudinal_distance : forwards or backwards
         longitudinal_distance = data_xyz.point.x
         lateral_distance = data_xyz.point.y
 
-        # get angle between the robot and the target position
         resultant_distance = math.sqrt(longitudinal_distance**2 + lateral_distance**2)
         angle = math.acos(longitudinal_distance/(resultant_distance))    #in radian
 
